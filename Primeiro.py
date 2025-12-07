1815db5c1b8b482d6853173b866d4912b6fc6c5c
Marcelo Bartz <bartz.dev.ia@gmail.com>
2025-12-06 21:58:53 -0300

    Aula 04/12/2025

--- Primeiro.py
+++ Primeiro.py
@@ -1,5 +1,64 @@
-print("Hello, World!")
+# Isto é um comentário.
+'''também é comentário ''' # Apostofre - aspas simples.
+""" Também é comentário""" # Aspas duplas.
+ 
+print("Tipos de Dados e Variáveis")
+
+x = 10 # Atribuição
+y = 7
+z = x+ y
+print("O resultado é: ", z)
+print()
+print("TIPO STRING")
+print("Sou uma String.")
+print("Sou outra String.")
+print()
+print("Tipo int e float")
+print(8, "int")
+print(3.14, 'float')
+print()
+print("Operações Aritméticas com int e float")
 x = 5
 y = 10
+print()
+print( x+y," adição")
+print()
+print( x-y," subtração")
+print()
+print( x*y," multiplicação")
+print()
+print( x/y," divisão")
+print()
+print( x**y," potenciação")
+print()
+print("UTILIZANDO OPERADORES DE ATRIBUIÇÃO")
+print()
+x += y  # mesmo que x = x + y
+print(x)
+print()
+x -= y  # mesmo que x = x - y
+print(x)  
+print()
+x *= y  # mesmo que x = x * y
+print(x)
+print()
+x /= y
+print(x)
+print()
+x %= y 
+print(x)
+print()
 
-print(x+y)
+
+
+
+
+
+
+
+
+
+
+
+
+
