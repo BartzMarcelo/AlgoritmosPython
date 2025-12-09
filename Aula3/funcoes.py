da3fdc7d9d7e31b67bb5c3abe8723a4e483050a5
MarceloBartz <bartz.dev.ia@gmail.com>
2025-12-08 21:38:13 -0300

    m2

--- Aula3/funcoes.py
+++ Aula3/funcoes.py
@@ -35,3 +35,21 @@ print(math.fmod(5,4))
 print( math.trunc(5.2))
 # Eleva o primeiro número ao segundo
 print ( math.pow(2,9))
+# Retorna a raiz quadrada
+print( math.sqrt(2))
+
+### Valores Especiais ###
+print( math.pi) # Valor de pi
+print(math.e) # Valor de Euler
+print( math.exp(4))# Euler elevado a n
+print( math.log(1000,10)) # Retorna o logaritmo de um número pelo outro
+print( math.log10(1000)) # Retorna o log 10 de um número 
+#help(print)
+grito = " Eu ESTOU GRITANDO!"
+print( grito.lower()) # Passa para caixa baixa9 normal)
+normal= " eu estou falando normalmente!"
+print(normal.upper())# Passa para caixa alta
+print(grito.capitalize()) # Somente a primeira letra maiúscula da frase
+troca =' essa É UMA strING emBARALHADA' 
+print( troca.swapcase()) # Inverte letra maiúscula para minúscula e vice-versa
+espacos = '-', '    espacoso    '
