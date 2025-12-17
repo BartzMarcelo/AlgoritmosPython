43e28dbe302d1830a39615990e77f99b1a3190c9
MarceloBartz <bartz.dev.ia@gmail.com>
2025-12-16 21:28:03 -0300

    1612

--- LacoDeRepeticao/001-for.py
+++ LacoDeRepeticao/001-for.py
@@ -14,4 +14,25 @@ print('-----------------------')
 for i in range(2,21,2):
     print('i =', i)
 print('-----------------------')
-    
+# Decremento.
+for i in range(9, -1, -1):
+    print('i =', i)
+print('-----------------------')
+for i in range(5,1):
+    print('i =', i) 
+print('-----------------------')
+# lista = []
+# for i in range(3):
+#     texto = input('Digite algo: ')
+#     lista.append(str(i) + ':' + texto)
+# print(lista)
+print('-----------------------')
+# Casting para list do range.
+numeros = range(0,10)
+print(type(numeros))
+print(numeros)
+# Convertendo para uma lista
+lista_num = list(numeros)
+print(type(lista_num))
+print(lista_num)
+
