b7306b1b56c8056af9cbd3cabbb4d8de1345ffaa
MarceloBartz <bartz.dev.ia@gmail.com>
2025-12-16 21:01:22 -0300

    1612

---
+++ LacoDeRepeticao/001-for.py
@@ -0,0 +1,17 @@
+for i in [1, 2, 3, 4 ,5]:
+    print('i =', i)
+print('')
+planetas = ['Mercurio', 'Venus', 'Terra', 'Marte', 'Jupiter', 'Saturno', 'Urano', 'Netuno']   
+print(planetas)
+for i in planetas:
+    print('i =', i)
+print('')
+
+# range(inicio, fim, passo)
+for i in range(5):
+    print('i =', i) 
+print('-----------------------')
+for i in range(2,21,2):
+    print('i =', i)
+print('-----------------------')
+    
