207e4ae2530b8d4a74cec536bb56198bb6aa27ae
Marcelo Bartz <bartz.dev.ia@gmail.com>
2026-02-11 07:34:52 -0300

    ed1 110226

--- Aula15_Ex.POO/ex003/classFuncionario.py
+++ Aula15_Ex.POO/ex003/classFuncionario.py
@@ -1,13 +1,17 @@
 class Funcionario:
 
-    salario_minimo = float(1412.00)
+    salario_minimo = float(1620.00)
     
     
     def __init__(self, nome: str, salario: float):
         self.nome = nome
         self.salario = salario
 
-    def verificar_salario_minimo(self) -> bool:
-        """Retorna True se o salário for maior ou igual ao mínimo da classe."""
+    def verificar_salario_minimo(self) -> str:
+        
         return self.salario >= Funcionario.salario_minimo
+    
+    def __str__(self)-> str:
+        return  f"A área do círculo com raio {self.raio} é {self.calcular_area():.2f}."
+
     
