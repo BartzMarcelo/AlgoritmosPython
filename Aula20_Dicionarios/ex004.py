ec1ab314dd7ad6fa266d1cb9105a0c3c5578eba1
LUIS MARCELO BARTZ DE AVILA <00499828003@senacrs.edu.br>
2026-03-02 19:44:12 -0300

    Aula 02/03/2026

--- Aula20_Dicionarios/ex004.py
+++ Aula20_Dicionarios/ex004.py
@@ -27,22 +27,33 @@ print()
 # Adicione um novo modelo chamado "Modelo_D" ao dicionário, mas faça isso solicitando que o usuário (ou simulando uma entrada) forneça um dicionário com as mesmas três chaves de métricas.
 
 
-print("--- Cadastro do Modelo_D ---")
+print("--- Cadastro do Modelo_D ---\n")  
 
 novas_metricas = {
     "acuracia": float(input("Digite a acurácia : ")),
     "tempo_treino": int(input("Digite o tempo de treino em segundos: ")),
-    "memoria": input("Digite o valor da memória com GB: ")
+    "memoria": input("Digite o valor da memória com GB: " )
 }
 
 experimentos["Modelo_D"] = novas_metricas
-print() 
-print(f"Adição de novo modelo ao dicionário: {experimentos['Modelo_D']}")
-print()  
-
+print()
+print(f"Modelo_D: {experimentos['Modelo_D']}" ) 
+print()   
 
 print(f"Experiementos atualizado: {experimentos}")
 print()
+maior_acuracia_modelo = None
+maior_acuracia = 0
+
+for modelo, metricas in experimentos.items():
+  
+    if metricas["acuracia"] > maior_acuracia:
+        maior_acuracia = metricas["acuracia"]
+        maior_acuracia_modelo = modelo
+print()
+print(f' A maior acurácia está no { maior_acuracia_modelo}')
+print()
+
 
 
 
